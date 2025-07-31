cbf07f00381040390c8753ea7d2922bbcc07bcf4
Erissonteixeira <seu-email-erisson.trabalhos@gmail.com>
2025-07-31 14:27:55 -0300

    feat: implements findById, updatePerson and deletePerson methods in PersonService

--- src/main/java/io/github/Erissonteixeira/api_crudpessoa/service/PersonService.java
+++ src/main/java/io/github/Erissonteixeira/api_crudpessoa/service/PersonService.java
@@ -1,5 +1,6 @@
 package io.github.Erissonteixeira.api_crudpessoa.service;
 
+import io.github.Erissonteixeira.api_crudpessoa.model.Address;
 import io.github.Erissonteixeira.api_crudpessoa.model.Person;
 import io.github.Erissonteixeira.api_crudpessoa.repository.PersonRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -17,5 +18,24 @@ public class PersonService {
     }
     public List<Person> findAll(){
         return repository.findAll();
+    }
+    public Person findById(Long id){
+        return repository.findById(id)
+                .orElseThrow(() -> new RuntimeException("Pessoa não encontrada com id: " + id));
+    }
+        public  Person updatePerson(Long id, Person novaPessoa){
+        Person person = findById(id);
+        person.setNome(novaPessoa.getNome());
+        person.setDataNascimento(novaPessoa.getDataNascimento());
+        person.setCpf(novaPessoa.getCpf());
+        person.getAddresses().clear();
+        for (Address endereco : novaPessoa.getAddresses()){
+            endereco.setPerson(person);
+            person.getAddresses().add(endereco);
+        }
+        return repository.save(person);
+    }
+    public void deletarPerson(Long id){
+        repository.deleteById(id);
     }
 }
