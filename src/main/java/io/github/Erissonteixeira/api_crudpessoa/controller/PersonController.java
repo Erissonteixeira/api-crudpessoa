4ad89b482266fa266be409ecd25a2ab445957129
Erissonteixeira <seu-email-erisson.trabalhos@gmail.com>
2025-08-14 16:44:19 -0300

    refactor: adjust endpoint names and standardize service methods and variables

--- src/main/java/io/github/Erissonteixeira/api_crudpessoa/controller/PersonController.java
+++ src/main/java/io/github/Erissonteixeira/api_crudpessoa/controller/PersonController.java
@@ -1,7 +1,10 @@
 package io.github.Erissonteixeira.api_crudpessoa.controller;
 
+import io.github.Erissonteixeira.api_crudpessoa.dto.PersonRequestDTO;
+import io.github.Erissonteixeira.api_crudpessoa.dto.PersonResponseDTO;
 import io.github.Erissonteixeira.api_crudpessoa.model.Person;
 import io.github.Erissonteixeira.api_crudpessoa.service.PersonService;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -10,32 +13,32 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/person")
+@RequestMapping("/persons")
 public class PersonController {
     @Autowired
     private PersonService service;
 
     @PostMapping
-    public ResponseEntity<Person> save(@RequestBody Person person){
-        var savePerson = service.savePerson(person);
-        return ResponseEntity.status(HttpStatus.CREATED).body(savePerson);
+    public ResponseEntity<PersonResponseDTO> save(@RequestBody @Valid PersonRequestDTO dto){
+        PersonResponseDTO savedPerson = service.createPerson(dto);
+        return ResponseEntity.status(HttpStatus.CREATED).body(savedPerson);
     }
     @GetMapping
-    public ResponseEntity<List<Person>> findAll(){
-        var people = service.findAll();
+    public ResponseEntity<List<PersonResponseDTO>> findAll(){
+        List<PersonResponseDTO> people = service.findAll();
         return ResponseEntity.ok(people);
     }
     @GetMapping("/{id}")
-    public ResponseEntity<Person> findById(@PathVariable Long id){
+    public ResponseEntity<PersonResponseDTO> findById(@PathVariable Long id){
         return ResponseEntity.status(HttpStatus.OK).body(service.findById(id));
     }
     @DeleteMapping("/{id}")
     public ResponseEntity<Void> deleteById(@PathVariable Long id){
-        service.deletarPerson(id);
+        service.deletePerson(id);
         return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
     }
     @PutMapping("/{id}")
-    public ResponseEntity<Person> update(@PathVariable Long id, @RequestBody Person person){
-        return ResponseEntity.status(HttpStatus.OK).body(service.updatePerson(id, person));
+    public ResponseEntity<PersonResponseDTO> update(@PathVariable Long id, @RequestBody @Valid PersonRequestDTO dto){
+        return ResponseEntity.ok(service.updatePerson(id, dto));
     }
 }
